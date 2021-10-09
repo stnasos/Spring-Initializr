78f10fb4ef54a4e7d3d29980541a9cf6d84993ce
GitHub Action <action@github.com>
2021-10-09 12:45:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/stnasos/springinitializr/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stnasos/springinitializr/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stnasos.springinitializr.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stnasos.springinitializr.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/stnasos/springinitializr/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stnasos/springinitializr/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stnasos.springinitializr.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stnasos.springinitializr.MyBundle
 
 class MyProjectService(project: Project) {
 
